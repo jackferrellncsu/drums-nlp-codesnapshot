9507a09a47e55d56f4de51133bffb658776ed17f
jackferrellncsu <jferrel3@ncsu.edu>
2021-10-01 11:58:40 -0400

    finish demo file

--- mlm_demonstration.py
+++ mlm_demonstration.py
@@ -3,33 +3,27 @@ import numpy
 import pickle
 import os
 import json
+import numpy
 import demonstration_routine as helper
+from torch.nn import Softmax 
+from transformers import BertForMaskedLM
+
 
 
 alphas = helper.load_cal_alphas("soft_alphas_0_0")
 
-from transformers import BertTokenizer, BertForMaskedLM
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+
+
 #initialize pre-trained model
 model =  BertForMaskedLM.from_pretrained("bert-base-uncased")
-
-test = tokenizer("Home entertainment [MASK] up", return_tensors='pt')
-
-outputs = model(**test)
-
+text = "Home entertainment [MASK] up"
+confidence = 0.75
+m_ind = 3
 
 
-#Input clean sentences.  
-#Words should already be masked.
-def conf_pred(sentence, model, conf, calib):
-    q_soft = numpy.quantile(calib, conf)
 
-    conf_intervals = []
-
-    input = tokenizer(sentence, return_tensors= 'pt', max_length=128)
-    input['labels'] = input.input_ids.detatch().clone()
-
-    outputs = model(**input)
+helper.conf_pred(text, model, confidence, alphas)
 
 
 
