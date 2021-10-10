63afa5e68db2e678adce0e71b2c907250520409c
jonathanpw <33842796+jonathanpw@users.noreply.github.com>
2021-10-09 21:45:52 -0400

    Update workflow-unix.sh

--- workflow-unix.sh
+++ workflow-unix.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 SEED=0
 
-#All python dependencies may be found in requirement.txt
-#All julia dependencies may be foind in Manifest.toml
-#Creates new environment called ".venv"
-python -m venv .venv
+# All python dependencies may be found in requirement.txt
+# All julia dependencies may be foind in Manifest.toml
+# Creates new environment called ".venv"
+# Note: Python 3 is required for the following lines of code
+python3 -m venv .venv
 
 #Activates created environment
 source .venv/bin/activate
 
 #Installs required python packages to new environment
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 #Creates data used in all methods
 #Stores in Data folder
-python src/run_mlm_datapr.py -s $SEED
+python3 src/run_mlm_datapr.py -s $SEED
 
 #Runs MLM conformal prediction process 5 times as reported in paper.
 #Takes several hours to complete.
-python src/run_mlm_bert.py -s $SEED
+python3 src/run_mlm_bert.py -s $SEED
 
 #Processes results of MLM conformal prediction process.
 #Creates the information presetned in tables 3 & 4, and figures 9, 10 , 11
-python src/out_mlm.py
+python3 src/out_mlm.py
 
 ####################
 # The following line produces the trained BiLSTM model along with the results
@@ -39,17 +40,17 @@ julia --project=. src/out_blstm.jl
 # Python 3.6 Enviroment
 source ".venv_pos/bin/activate"
 
-python -m pip install -r req_pos.txt
+python3 -m pip install -r req_pos.txt
 
 #Function used for following python files
-python src/routine_bertpos.py
+python3 src/routine_bertpos.py
 
 #This creates all 5 BERT models based on the 5 train test spilts (Days)
 #Saves all 5 models from our training labeled bert_taggeri.h5
-python src/run_bertpos.py
+python3 src/run_bertpos.py
 
 #Calculates the calibration and p-values, give results or metrics and generates plots. (Hours)
 #Final Results in BPS section of the table is called RESULTS.CSV used in tables 1 & 2
 
-python src/BERT_out.py
+python3 src/BERT_out.py
 
