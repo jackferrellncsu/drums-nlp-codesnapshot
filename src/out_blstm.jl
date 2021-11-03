41368bd200bfbbc210f57ac1cf0696b9bd948df7
eplanch <eplanch@ncsu.edu>
2021-11-03 18:23:38 -0400

    graph update to out_blstm

--- src/out_blstm.jl
+++ src/out_blstm.jl
@@ -19,7 +19,7 @@ sizes_999 = get(plot_requirements, "sizes_999", 0)
 # ------------------------ Saving plots and figures ------------------------ #
 
 # Finds and plots the distribution of nonconformity scores
-nonconf_dist = histogram(α_i, bins = 0:.022:1, color = :red, leg = false,
+nonconf_dist = histogram(α_i, bins = 0:.022:1, color = :black, leg = false,
                             grid = false, linecolor = false,
                             yticks = ([0:20000:100000;],
                             ["0", "20000", "40000", "60000", "80000", "100000"]))
@@ -29,7 +29,7 @@ savefig(nonconf_dist, "out/out_blstm_results/blstm_nonconf_dist.png")
 # Finds and plots the closer distribution of nonconformity scores
 ranger = ranged_α(α_i)
 close_nonconf_dist = histogram(ranger, bins = 0:8.547008547008547e-7:.0002,
-                                color = :red, leg = false, grid = false,
+                                color = :black, leg = false, grid = false,
                                 linecolor = false, yticks = ([0:2000:8000;],
                                 ["0", "2000", "4000", "6000", "8000"]),
                                 xticks = ([0:.0002:.0002;], ["0", "0.0002"]))
@@ -37,13 +37,13 @@ close_nonconf_dist = histogram(ranger, bins = 0:8.547008547008547e-7:.0002,
 savefig(close_nonconf_dist, "out/out_blstm_results/blstm_nonconf_dist_close.png")
 
 # Finds and plots the empirical vs proposed validity
-emp_prop = plot(confs, nominal, leg = false, grid = false)
-emp_prop = plot!(confs, confs)
+emp_prop = plot(confs, nominal, leg = false, grid = false, color = :black)
+emp_prop = plot!(confs, confs, color = :black, linestyle = :dash)
 
 savefig(emp_prop, "out/out_blstm_results/blstm_emp_prop.png")
 
 # Finds and plots the distribution of set sizes for 99.9% confidence
-set_sizes999 = histogram(sizes_999, leg = false, bins = 141, color = :red, grid = false,
+set_sizes999 = histogram(sizes_999, leg = false, bins = 141, color = :black, grid = false,
                             linecolor = false, yticks = ([0:20000:40000;],
                             ["0", "20000", "40000"]), xticks = ([0:20:140;],
                             ["0", "20", "40", "60", "80", "100", "120", "140"]))
