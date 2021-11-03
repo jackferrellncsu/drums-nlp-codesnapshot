60533c48499c4f532315bb262b117e1465868fd9
mlovig <84802520+mlovig@users.noreply.github.com>
2021-11-03 16:55:07 -0500

    Update out_bertpos.py

--- src/out_bertpos.py
+++ src/out_bertpos.py
@@ -157,7 +157,7 @@ for zzz in range(0,5):
 
 #Distribution of prediction set sizes
 NSIZES = intervalDist(pVals, .01)
-mpl.hist(NSIZES, bins = range(0,max(NSIZES)+1), color = 'red')
+mpl.hist(NSIZES, bins = range(0,max(NSIZES)+1), edgecolor = 'black', color = 'white')
 mpl.xlim((0,max(NSIZES)))
 mpl.xticks([0,5,10,15])
 mpl.savefig("out/out_bertpos_results/PredSets99.png")
@@ -171,14 +171,14 @@ for i in tqdm(range(1, 100)):
     nomconf.append(1- i/100)
     empconf.append(confNPISACDS(pVals, trueLabels, i/100)[0])
 
-mpl.plot(nomconf, empconf, label = "BERT")
-mpl.plot(np.array(range(1,100))/100, np.array(range(1,100))/100, label = "Nominal")
+mpl.plot(nomconf, empconf, color = 'black')
+mpl.plot(np.array(range(1,100))/100, np.array(range(1,100))/100, color = 'black', linestyle = 'dashed')
 mpl.savefig("out/out_bertpos_results/NominalVEmpirical.png")
 
 mpl.close()
 
 #Distribution of Non-Conformity Scores
-mpl.hist(flat_calib, color = 'red', bins = int(np.sqrt(len(flat_calib))/7.5))
+mpl.hist(flat_calib, edgecolor = 'black', color = 'white', bins = int(np.sqrt(len(flat_calib))/7.5))
 mpl.savefig("out/out_bertpos_results/NonConfHist.png")
 
 mpl.close()
@@ -189,7 +189,7 @@ for x in flat_calib:
     if x < .0002:
         trim_calib.append(x)
 
-mpl.hist(trim_calib, color = 'red', bins = int(np.sqrt(len(trim_calib))))
+mpl.hist(trim_calib, edgecolor = 'black', color = 'white', bins = int(np.sqrt(len(trim_calib))))
 mpl.xlim(0,.0002)
 mpl.xticks([0,.0002])
 mpl.savefig("out/out_bertpos_results/NonConfZoom.png")
